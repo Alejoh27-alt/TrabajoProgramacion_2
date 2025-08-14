38dc06844ff7cebcd64c891af13e6a76ee874558
Alejoh27-alt <josea.hernandezh@uqvirtual.edu.co>
2025-08-14 00:36:45 -0500

    Creacion de Objetos
    Se crearon algunos objetos de prueba

--- co.edu.uniquindio.trabajoUno/src/main/java/co/edu/uniquindio/trabajoUno/Main.java
+++ co.edu.uniquindio.trabajoUno/src/main/java/co/edu/uniquindio/trabajoUno/Main.java
@@ -1,17 +1,21 @@
 package co.edu.uniquindio.trabajoUno;
 
+import model.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        EmpresaTransporte nombreEmpresa = new EmpresaTransporte("La Carreta");
+        Propietario propietarioUno = new Propietario("Jose","jose@gmail.com", "33333", "111");
+        Usuario usuarioUno = new Usuario(18);
+        VehiculoCarga vehiculoCargaUno = new VehiculoCarga(18.2, 7);
+        VehiculoTransporte vehiculoTransporteUno = new VehiculoTransporte(8);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(nombreEmpresa);
+        System.out.println(propietarioUno);
+        System.out.println(usuarioUno);
+        System.out.println(vehiculoCargaUno);
+        System.out.println(vehiculoTransporteUno);
     }
 }
